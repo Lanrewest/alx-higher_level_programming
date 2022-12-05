21ecf7a6600148ae413eb00edc14b79793e5d61d
Lanrewest <lanrewese@yahoo.com>
2022-12-05 12:37:09 -0800

    mm

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,12 +5,12 @@ def print_matrix_integer(matrix=[[]]):
         print()
     else:
         tam = len(matrix[0])
-        cont = 0
+        count = 0
         for int in matrix:
             for int2 in int:
-                cont += 1
-                if cont == tam:
+                count =count + 1
+                if count == tam:
                     print("{:d}".format(int2), end="\n")
                 else:
                     print("{:d} ".format(int2), end="")
-            cont = 0
+            count = 0
