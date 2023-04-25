5dd2ec51a20ce4e3504814aeb2cdc5b6b5f54ae7
lanrewest <lanrewese@yahoo.com>
2023-04-25 13:17:15 -0700

    task

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
+});
+
+
