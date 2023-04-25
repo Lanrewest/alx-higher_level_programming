cdbc4f2e29d9fa31bca46ce1465d803a146274e7
lanrewest <lanrewese@yahoo.com>
2023-04-25 12:38:11 -0700

    java

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, content) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(content);
+});
+
