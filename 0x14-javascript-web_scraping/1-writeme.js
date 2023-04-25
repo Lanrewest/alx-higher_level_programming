67d3af60c60280b960c7adeeb1a41cf134806e06
lanrewest <lanrewese@yahoo.com>
2023-04-25 13:00:12 -0700

    writeme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(`Successfully wrote "${contentToWrite}" to ${filePath}.`);
+});
+
